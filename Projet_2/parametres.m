718fefc6ae04488d0574bb357f07991943cfa2ed
Alexandre Lenaerts <13055@ecam.be>
2018-06-14 19:36:35 +0200

    Update parametres.m

--- Projet_2/parametres.m
+++ Projet_2/parametres.m
@@ -18,7 +18,7 @@ span = 20;       % rcos span for thinner bandwidth consumption
 %pwr = 200;       % channel power in mW
 
 % Canal 
-Eb_No = 30;                                     %SNR
+Eb_No = 30;                                     % SNR
 random_delay = randi([0 Tb/Tn],1,K)             % Délais aléatoire
 %random_alphaN = (rand(1,1))                    % facteur d-affaiblissement aléatoire (>1)
 alpha_N=0.97;                                   % facteur d-affaiblissement fixe (>1)  
